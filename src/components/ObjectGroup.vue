0bc5193fd78b7aab85086f1b7c6d34a38186336a
duyenht <duyenht@vng.com.vn>
2020-07-31 16:42:36 +0700

    .. (reverted from commit 5670e311467fc4b446461bcf1f5e9c764494e9a4)

--- src/components/ObjectGroup.vue
+++ src/components/ObjectGroup.vue
@@ -147,7 +147,6 @@
   import TimeUtil from '../Utility/TimeUtility';
   import ERROR_CODE from '../const/error_code';
   import CHANNEL_PAYMENT from '../const/channel_const';
-  import GameMixin from '../mixin/gameMixin';
   import moment from 'moment';
   import router from '../router';
   import GameData from '../Utility/GameData';
@@ -159,7 +158,6 @@
 
   export default {
     name: 'ObjectGroup',
-    mixins: [GameMixin],
     components: {
       Dropdown,
       Navigation,
